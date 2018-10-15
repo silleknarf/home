ae95ab0eaf1446f758649883a81decd9107df45d
silleknarf@gmail.com <silleknarf@gmail.com>
2018-10-15 23:16:46 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Navbar,
+  NavbarBrand,
+  ListGroup, 
+  ListGroupItem } from 'reactstrap';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPlay)
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { activities: [] };
+  }
+
+  componentDidMount() {
+    const activitiesUrl = "http://localhost:8282/hubs/harmony-hub/activities";
+    fetch(activitiesUrl)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ activities: data.activities });
+      })
+  }
+
+  onStartActivity(activity) {
+    console.log(`Starting activity: ${activity.label}`);
+    // eslint-disable-next-line 
+    const activitiesPostUrl = `http://localhost:8282/hubs/harmony-hub/activities/${activity.slug}`;
+    //fetch(activitiesPostUrl, { method: "POST" });
+  }
+
   render() {
+    const activitiesList = this.state.activities
+      .map(a => (
+        <ListGroupItem className="App-list-item">
+          {a.label}
+          <span className="App-icon">
+            <FontAwesomeIcon icon="play" size="2x" onClick={() => this.onStartActivity(a)} />
+          </span>
+        </ListGroupItem>));
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar className="App-nav">
+          <NavbarBrand>
+            Home
+          </NavbarBrand>
+        </Navbar>
+        <ListGroup>
+          { activitiesList }
+        </ListGroup>
       </div>
     );
   }
